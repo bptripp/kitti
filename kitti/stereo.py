78c65cb3c025f0d49a11892ef7894c4980ef0e00
Bryan <bptripp@uwaterloo.ca>
2015-10-14 16:03:09 -0400

    Changing load functions to handle multiview

--- kitti/stereo.py
+++ kitti/stereo.py
@@ -3,20 +3,20 @@ import os
 from kitti.data import data_dir
 
 
-def load_image(index, test=False, right=False, future=False):
+def load_image(index, test=False, right=False, frame=10, multiview=False):
     import scipy.ndimage
     path = os.path.join(
         data_dir,
-        'data_stereo_flow',
+        'data_stereo_flow_multiview' if multiview else 'data_stereo_flow',
         'testing' if test else 'training',
         'image_1' if right else 'image_0',
-        "%06d_%2d.png" % (index, 11 if future else 10))
+        "%06d_%02d.png" % (index, frame))
     return scipy.ndimage.imread(path)
 
 
-def load_pair(index, test=False, future=False):
-    left = load_image(index, right=False, test=test, future=future)
-    right = load_image(index, right=True, test=test, future=future)
+def load_pair(index, test=False, frame=10, multiview=False):
+    left = load_image(index, right=False, test=test, frame=frame, multiview=multiview)
+    right = load_image(index, right=True, test=test, frame=frame, multiview=multiview)
     return left, right
 
 
